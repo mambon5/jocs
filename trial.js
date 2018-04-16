b4ef670103406a71882c8633c172555255aa373e
mambon5 <monantoniet@gmail.com>
2018-04-16 18:41:14 +0200

    square of numbers
    tables that compute the square of numbers from 14 up to 63 in a user-friendly way

---
+++ trial.js
@@ -0,0 +1,25 @@
+function loadcraziness(ini=14,fi=33) {
+			var rand = _.sample(_.range(ini,fi),10);
+			for(i=1; i<11; ++i) {
+				id1 = "n"+i;
+				document.getElementById(id1).innerHTML = rand[i-1];
+				document.getElementById(id1).style.top = document.getElementById("lab").getBoundingClientRect().top + i*50 + "px";
+				document.getElementById(id1).style.visibility="visible";
+				id2 = id1 + "sol";
+				document.getElementById(id2).innerHTML = "solution";
+				document.getElementById(id2).style.top = document.getElementById("lab").getBoundingClientRect().top + i*50 +10 + "px";
+				document.getElementById(id1).style.left = document.getElementById("lab").getBoundingClientRect().left + "px";
+				document.getElementById(id2).style.left = document.getElementById("labsol").getBoundingClientRect().left + "px";
+				document.getElementById(id2).style.visibility="visible";
+				}
+			document.getElementById("load").innerHTML = "load";
+		}
+
+		function aux(){			
+			var x = [1,2,3,4,5,6,7];			
+			var rnd5 = _.uniq(_.sample(x,5));		
+			res="";
+			for(i=0; i<rnd5.length; ++i) res= res + " " + rnd5[i]; 
+			i=3;
+			document.getElementById("aux").innerHTML = 10 + 20*3 + "px";
+		}
